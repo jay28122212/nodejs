241ef9841aa52b2b0852ba406c36b6094b63fb4a
jay28122212 <95669684+jay28122212@users.noreply.github.com>
2021-12-27 17:25:55 +0530

    Add files via upload

---
+++ module/app.js
@@ -0,0 +1,11 @@
+console.log('app.js')
+
+const name='jay'
+
+const add=function(a,b){
+
+    console.log(process.argv);
+    return a+b
+}
+
+module.exports=add
--- module/module.js
+++ module/module.js
@@ -1,1 +1,46 @@
+// File system
+ const fs = require ('fs')
 
+ const yargs =require('yargs')
+
+ fs.writeFileSync('note.txt','my first progrem in node.js')
+
+fs.appendFileSync('note.txt', 'And i learn Fast')
+
+// Sum for value Using Export
+
+const add= require('./app.js')
+const sum=add(4,8)
+ console.log(sum)  
+
+
+//Validator in Npm
+
+const validator= require('validator')
+
+ console.log(validator.isEmail('jaypatel@'))
+
+
+
+// Chalk in Npm
+
+// const chalk = require ('chalk')
+
+// const green = chalk.green('succes!')
+// console.log(green)
+
+//  Process and Argv
+
+ console.log(process.argv)
+
+const command=process.argv[2]       
+if(command==='add'){
+    console.log('hfhfhfhfh')
+}else if(command==='remove')
+{
+console.log('removing')
+}
+
+// Yargs
+
+console.log(yargs.argv)
